7fe3a4d74a98eb0ff998fd450f4b1aca2e25a692
Kelvin <ngotho.kelvin@yahoo.com>
2024-02-25 00:08:36 +0300

    Add overlay

--- app/components/home/home-1/CarIntro.js
+++ app/components/home/home-1/CarIntro.js
@@ -7,40 +7,18 @@ const CarIntro = () => {
 
   return (
     <>
-      <ModalVideo
-        channel="youtube"
-        autoplay
-        isOpen={isOpen}
-        videoId="ol9ixt74uE8"
-        onClose={() => setOpen(false)}
-      />
-
       <div className="row">
-        <div className="col-lg-12">
-          <div className="posr">
-            <div className="home1_divider_video_pop">
-              <div className="video_popup_icon">
-                <button
-                  className="video_popup_btn popup-img popup-youtube"
-                  role="button"
-                  onClick={() => setOpen(true)}
-                >
-                  <span className="flaticon-play" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* End .col-lg-12 */}
-
         <div className="col-md-12 col-xl-9">
           <div className="home1_divider_content">
             <h2 className="title" style={{ color: '#1a3760' }}>We Make Finding Land Simple</h2>
-            <p className="para">
-              At Rascom what matters to us is making your land search and buying
-              experience as simple as possible, so you can find the right land
-              quickly and get on with making it yours.
-            </p>
+            <div className="containerx">
+              <div className="misty-overlay"></div>
+              <p className="para misty-text" style={{ color: '#1a3760' }}>
+                At Rascom what matters to us is making your land search and buying
+                experience as simple as possible, so you can find the right land
+                quickly and get on with making it yours.
+              </p>
+            </div>
           </div>
         </div>
         {/* End col-md-9 */}
