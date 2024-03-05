7fa898a2ddd86a5be54bf17df1ddd40c741287aa
kelvin <ngotho.kelvin@yahoo.com>
2024-03-05 08:58:24 +0300

    Add footer header

--- app/components/common/footer/Navigation.js
+++ app/components/common/footer/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
     { label: "Home", path: "/" },
     { label: "About Us", path: "/about-us" },
     { label: "Listings", path: "/listing-v1" },
-    { label: "Blog", path: "/blog" },
-    { label: "User", path: "/user-profile" },
-    { label: "Service", path: "/service" },
+    { label: "Blog", path: "/blog-grid" },
+    // { label: "User", path: "/user-profile" },
+    // { label: "Service", path: "/service" },
   ];
 
   return (
